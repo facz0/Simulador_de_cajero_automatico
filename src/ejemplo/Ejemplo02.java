e8bdd6c793683d13e8131881f476feba395ce410
facz0 <fabriziocossio99@gmail.com>
2026-01-25 22:59:11 -0500

    Agregación de nueva clase

---
+++ src/ejemplo/Ejemplo02.java
@@ -0,0 +1,8 @@
+package ejemplo;
+
+public class Ejemplo02 {
+	public static void main(String[] args) {
+		System.out.print("Hola, este es un cambio de Fabrizio");
+		
+	}
+}
--- src/ejemplo/PRUEBA.java
+++ src/ejemplo/PRUEBA.java
@@ -4,6 +4,7 @@ public class PRUEBA {
 	
 	public static void main(String[] args) {
 		System.out.print("Hola Mundo");
+		
 	}
 	
 }
