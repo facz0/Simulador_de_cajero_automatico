263e886c9051b8d2c603f947dfd328f9175b9595
facz0 <fabriziocossio99@gmail.com>
2026-01-29 11:06:43 -0500

    creacion parcial de login como ejemplo

--- src/gui/Login_administrador.java
+++ src/gui/Login_administrador.java
@@ -3,11 +3,16 @@ package gui;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class Login_administrador extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private VentanaPrincipal ventanaPrincipal;
+	private JTextField textField;
+	private JTextField textField_1;
 	/**
 	 * Create the panel.
 	 * @param ventanaPrincipal 
@@ -21,6 +26,29 @@ public class Login_administrador extends JPanel {
 		panel.setBackground(new Color(192, 192, 192));
 		panel.setBounds(488, 113, 453, 525);
 		add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.setBounds(159, 293, 89, 23);
+		panel.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(85, 239, 203, 23);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("password");
+		lblNewLabel.setBounds(85, 206, 118, 23);
+		panel.add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(85, 172, 203, 23);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("DNI");
+		lblNewLabel_1.setBounds(85, 130, 61, 14);
+		panel.add(lblNewLabel_1);
 
 	}
 }
